f08207d97ba75c6182a34145f5b1cda5142897af
martautor <167963978+martautor@users.noreply.github.com>
2024-07-23 17:35:50 +0300

    Little Update

--- client/src/routes/Default.js
+++ client/src/routes/Default.js
@@ -2,13 +2,13 @@ import { Box, Button, Container } from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
 import '../components/css/default.css'
 export default function Default() {
-    return (<div>
+    return (<Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
         <Container sx={{textAlign: 'center'}}>
             <Link to='/home'> <Button>Home</Button>   </Link>
             <Link to='/play'> <Button>Let's play</Button> </Link>
         </Container>
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
             <Outlet />
         </Box>
-    </div>)
+        </Container>)
 }
--- client/src/routes/Play.js
+++ client/src/routes/Play.js
@@ -1,27 +1,40 @@
-import { Box, Button, Container, Paper } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { useEffect, useState } from "react";
 
 export default function Play() {
-    const [value, setValue] = useState(0)
+    
+    const [value, setValue] = useState(1)
     const [xpoint, setXpoint] = useState(1)
     const [radius, setRadius] = useState(0)
-
-    useEffect(() => {
-        blockRadiusUp()
-    })
+    const [color, setColor] = useState()
     
-
-    const addPoint = () => {
-        setValue(value + xpoint)
-    }
     const blockRadiusUp = () => {
-        setRadius(value + 0.5)
-    }
-    return (<Paper  onClick={addPoint} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '1000px', height: '1000px', background: grey[300], borderRadius: 5}}>
-                
+        setRadius(value + 1)
+        setValue(value + xpoint)
+        if(value < 10) {
+            setColor('#00ff0' + value)
+        } else if (value < 100) {
+            setColor('#00ff' + value)
+        } else if (value > 99) {
+            let sval = String(value)[1] + String(value)[2] 
+            if(value > 200 && value < 300) 
+                return setColor('#ffff' + sval)
+            if(value > 300) 
+                return setColor('#30ff' + sval)  
+            return setColor('#10ff' + sval)
+        } else if (value > 199 && value < 499) {
+            let sval = String(value)[1] + String(value)[2]
+            setColor('#10ff' + sval)
+        }
+        console.log(color)
+    } 
+    console.log('radius:', radius)
+
+    return (
+            <Paper  onClick={blockRadiusUp} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '75vw', height: '75vh', background: grey[300], borderRadius: 5}}>    
                 <h4>{value}</h4>
-                <Box onClick={blockRadiusUp} width={radius} height={radius} sx={{background: 'green', borderRadius: 10}} color='error'>
+                <Box width={radius + 'px'} height={radius + 'px'} sx={{background: color, borderRadius: 25}} color='error'>
                 </Box>
             </Paper>)
 }
